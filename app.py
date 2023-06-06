1fa8349fe817be890137ef259b032bafdffd05d3
Falix <30694778+falixchong@users.noreply.github.com>
2023-06-06 17:36:12 +0800

    Update dog size

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 import requests
 
 option_a = os.getenv('OPTION_A', u"Small Cat 🐺")
-option_b = os.getenv('OPTION_B', u"Small Dog 🐶")
+option_b = os.getenv('OPTION_B', u"Big Dog 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
